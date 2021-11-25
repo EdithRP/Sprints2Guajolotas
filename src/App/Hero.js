37d047483db09de79892773dc27efbd51ad2eef0
EdithRincon <andre-lin@hotmail.es>
2021-11-25 10:34:32 -0500

    Envio de informacion

--- src/App/Hero.js
+++ src/App/Hero.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { TodoList } from '../TodoList/index';
 import { TodoItem } from '../TodoItem/index';
